5dfffe17c04b6cfeb0d5de56135a1f3fee21a5d5
郑净云 <zhengjingyun@unionbigdata.com>
2020-01-15 16:39:55 +0800

    add springcloud gateway zuul

---
+++ springcloud-gateway/src/main/java/night/Application.java
@@ -0,0 +1,19 @@
+package night;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author night
+ * @version 1.1.0
+ * @Date 2020/1/3 10:57
+ */
+@SpringBootApplication
+@EnableZuulProxy
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
